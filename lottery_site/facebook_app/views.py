247f1af7176f13eb08da223350f45e69b3d210a8
Taras Nek <mr.taras1k@gmail.com>
2012-08-26 15:45:24 +0300

    template view to form view

--- lottery_site/facebook_app/views.py
+++ lottery_site/facebook_app/views.py
@@ -1,5 +1,5 @@
-from django.views.generic.base import TemplateView
+from django.views.generic import FormView
 
-class FacebookStartPage(TemplateView):
+class FacebookStartPage(FormView):
 
 	template_name = 'facebook_app/index.html'
